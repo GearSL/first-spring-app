32b05ecccd14fe7bcf3055a9c95bc35940051b30
Ilyas <i.ismailov@dengiclick.kz>
2022-08-09 16:29:51 +0600

    L6: Completed

--- src/main/java/kz/ibr/spring/MusicPlayer.java
+++ src/main/java/kz/ibr/spring/MusicPlayer.java
@@ -1,19 +1,16 @@
 package kz.ibr.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
-    //IoC
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
 
-    public MusicPlayer(){}
-
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
     public void setName(String name) {
         this.name = name;
@@ -21,8 +18,8 @@ public class MusicPlayer {
     public void setVolume(int volume) {
         this.volume = volume;
     }
-    public Music getMusic() {
-        return music;
+    public List<Music> getMusic() {
+        return musicList;
     }
 
     public String getName() {
@@ -32,7 +29,9 @@ public class MusicPlayer {
     public int getVolume() {
         return volume;
     }
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+    public void playMusicList() {
+        for(Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
